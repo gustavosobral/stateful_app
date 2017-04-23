d3a982f947f161bc7d89c93d04649d57bea92139
Gustavo Sobral <ghsobral@gmail.com>
2017-04-23 15:38:42 +0200

    Integrate states with API

--- src/states/states.list.controller.js
+++ src/states/states.list.controller.js
@@ -1,11 +1,14 @@
 var stateDialogTemplate = require('./state.dialog.template.html');
 
-function StatesListController($stateParams, ModelsService, CurrentUserService, ngDialog) {
+function StatesListController($stateParams, ModelsService, StatesService,
+                              CurrentUserService, ngDialog) {
   var vm = this;
-  vm.openDialog = openDialog;
+  vm.updateStates = updateStates;
+  vm.modelWalkState = modelWalkState;
   vm.removeState = removeState;
+  vm.openDialog = openDialog;
 
-  function loadData(modelId) {
+  function loadStates(modelId) {
     ModelsService.get(modelId)
       .then(function(response) {
         vm.model = response;
@@ -13,28 +16,44 @@ function StatesListController($stateParams, ModelsService, CurrentUserService, n
       });
   }
 
-  function openDialog() {
-    ngDialog.open({ templateUrl: stateDialogTemplate,
-                    className: 'states__dialog',
-                    preCloseCallback: function(value) {
-                      if(value && value != '$document' && value !='$closeButton') {
-                        addState(value);  
-                      }
-                    }
-    });
+  function updateStates() {
+    StatesService.update(vm.modelId, vm.states)
+      .then(function(response) {
+        activate();
+      });
   }
 
-  function addState(stateName) {
-    vm.states.push({ id: null, order: null, name: stateName });
+  function modelWalkState() {
+    ModelsService.walkState(vm.modelId)
+      .then(function(response) {
+        vm.model = response;
+        vm.states = vm.model.states;
+      });
   }
 
   function removeState(index) {
     vm.states.splice(index, 1);
   }
 
+  function addState(stateName) {
+    vm.states.push({ id: null, order: null, name: stateName });
+  }
+
+  function openDialog() {
+    ngDialog.open({ templateUrl: stateDialogTemplate,
+                    className: 'states__dialog',
+                    preCloseCallback: function(value) {
+                      if(value && value != '$document' && value !='$closeButton') {
+                        addState(value);
+                      }
+                    }
+    });
+  }
+
   function activate() {
     vm.currentUser = CurrentUserService.getCurrentUser();
-    loadData($stateParams.id);
+    vm.modelId = $stateParams.id;
+    loadStates(vm.modelId);
   }
 
   activate();
--- src/states/states.service.js
+++ src/states/states.service.js
@@ -1,12 +1,31 @@
 function StatesService($http, $q, $auth) {
   return {
-    get: get,
+    get:    get,
+    update: update
   };
 
   function get(modelId) {
     var defer = $q.defer();
 
     $http.get($auth.apiUrl() + '/models/' + modelId + '/states')
+      .then(function(response) {
+        defer.resolve(response.data);
+      })
+      .catch(function(response, code) {
+        defer.reject(response);
+      });
+
+    return defer.promise;
+  }
+
+  function update(modelId, states) {
+    var defer = $q.defer();
+
+    var data = {
+      states: states
+    };
+
+    $http.post($auth.apiUrl() + '/models/' + modelId + '/states', data)
       .then(function(response) {
         defer.resolve(response.data);
       })
